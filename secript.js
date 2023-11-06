951209eb90e74fbae4462f51ce84b728a360fdd5
HASAN <148566101+HASANMAHMUD25@users.noreply.github.com>
2023-11-06 20:30:52 +0700

    Add files via upload

---
+++ secript.js
@@ -0,0 +1,11 @@
+
+// ini variabel cek 
+let nama = prompt("masukan nama")
+
+let kehadiran = prompt("masukan 2 jika benar")
+
+let cek = confirm("apakah sudah benar")
+
+let hasil =  kehadiran == "2" ? "kamu benar" : "kamu salah"
+
+alert(hasil)
